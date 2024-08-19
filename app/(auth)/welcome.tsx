684c1c5d1507d9c504e690d2ad4b4b08fd864a17
Santosh Kumar <santoshkumar23kky@gmail.com>
2024-08-19 23:36:18 +0530

    welcome page complete

--- app/(auth)/welcome.tsx
+++ app/(auth)/welcome.tsx
@@ -1,3 +1,4 @@
+import CustomButton from "@/components/CustomButton";
 import { onboarding } from "@/constant";
 import { router } from "expo-router";
 import { useRef, useState } from "react";
@@ -7,7 +8,8 @@ import Swiper from "react-native-swiper";
 
 const Onboarding = () => {
   const swiperRef = useRef<Swiper>(null);
-  const [active, setActive] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
 
   return (
     <SafeAreaView className="flex h-full items-center justify-between bg-white">
@@ -28,7 +30,7 @@ const Onboarding = () => {
         activeDot={
           <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
         }
-        onIndexChanged={(index) => setActive(index)}
+        onIndexChanged={(index) => setActiveIndex(index)}
       >
         {onboarding.map((item, index) => (
           <View key={index} className="flex items-center justify-center p-5">
@@ -37,12 +39,26 @@ const Onboarding = () => {
               className="w-full h-[300px]"
               resizeMode="contain"
             />
-            <Text className="text-black text-lg font-JakartaBold">
-              {item.title}
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
             </Text>
           </View>
         ))}
       </Swiper>
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(auth)/sign-up")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
     </SafeAreaView>
   );
 };
